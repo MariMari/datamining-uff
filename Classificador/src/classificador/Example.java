84114a3e67adf58760ae2fb5d6d4a86a50d2ae22
Igor Giusti da Silva <igor@tuca.(none)>
2009-04-19 14:50:58 -0300

    Criados metodos para transformar valores de atributos extensos para numericos e vice-versa. Refactoring da classe Example para utilizacao daqueles metodos.

--- Classificador/src/classificador/Example.java
+++ Classificador/src/classificador/Example.java
@@ -4,59 +4,83 @@
  */
 package classificador;
 
+import static classificador.Utils.*;
+
 /**
  *
  * @author Marianna Portela
  * @author igorg
  */
 public class Example {
+    
+    //Constantes referentes aos valores dos dominios dos atributos discretos.
+    private static final String[] dominioSaldo = 
+            {"<0", "0<=X<200", ">=200", "no checking"};
+    
+    private static final String[] dominioHistCredito = 
+            {"no credits/all paid", "all paid", "existing paid",
+             "delayed previously", "critical/other existing credit"};
+    
+    private static final String[] dominioEconomias =
+            {"<100", "100<=X<500", "500<=X<1000", ">=1000", "no known savings"};
+    
+    private static final String[] dominioEstPessoal =
+            {"male div/sep", "female div/sep/mar", "male single",
+             "male mar/wid", "female single"};
+    
+    private static final String[] dominioClasse =
+            {"good", "bad"};
 
-    private int valor;
+    private double valor;
     private int duracao_emp;
-    private String saldo;
-    private String hist_credito;
-    private String economias;
-    private String est_pessoal;
-    private String classe;
+    private int saldo;
+    private int hist_credito;
+    private int economias;
+    private int est_pessoal;
+    private int classe;
 
-    Example(String lineRead) {
+    Example(String lineRead) throws IllegalArgumentException {
             String [] attributes = lineRead.split(",");
             
-            duracao_emp = Integer.parseInt(attributes[1]);
-            valor = Integer.parseInt(attributes[3]);
+            setValor(Double.parseDouble(attributes[3]));
             
-            saldo = attributes[0];
-            hist_credito = attributes[2];
-            economias = attributes[4];
-            est_pessoal = attributes[5];
-            classe = attributes[6];
+            setDuracao(Integer.parseInt(attributes[1]));
+            try {
+                setSaldo(attrStringToNumerical(dominioSaldo, attributes[0]));
+                setHistorico(attrStringToNumerical(dominioHistCredito,attributes[2]));
+                setEconomias(attrStringToNumerical(dominioEconomias, attributes[4]));
+                setEstPessoal(attrStringToNumerical(dominioEstPessoal, attributes[5]));
+                setClasse(attrStringToNumerical(dominioClasse, attributes[6]));
+            } catch (Exception e) {
+                throw new IllegalArgumentException(e.getMessage());
+            }
     }
     
     public int getDuracao() {
         return duracao_emp;
     }
     
-    public int getValor() {
+    public double getValor() {
         return valor;
     }
 
-    public String getSaldo() {
+    public int getSaldo() {
         return saldo;
     }
 
-    public String getHistorico() {
+    public int getHistorico() {
         return hist_credito;
     }
 
-    public String getEconomias() {
+    public int getEconomias() {
         return economias;
     }
 
-    public String getEstPessoal() {
+    public int getEstPessoal() {
         return est_pessoal;
     }
 
-    public String getClasse() {
+    public int getClasse() {
         return classe;
     }
     
@@ -64,39 +88,39 @@ public class Example {
         duracao_emp = duracao;
     }
     
-    public void setValor(int valor) {
+    public void setValor(double valor) {
         this.valor = valor;
     }
 
-    public void setSaldo(String saldo) {
+    public void setSaldo(int saldo) {
         this.saldo = saldo;
     }
 
-    public void setHistorico(String historico) {
+    public void setHistorico(int historico) {
         hist_credito = historico;
     }
 
-    public void setEconomias(String economias) {
+    public void setEconomias(int economias) {
         this.economias = economias;
     }
 
-    public void setEstPessoal(String estPessoal) {
+    public void setEstPessoal(int estPessoal) {
         this.est_pessoal = estPessoal;
     }
 
-    public void setClasse(String classe) {
+    public void setClasse(int classe) {
         this.classe = classe;
     }
     
     @Override
-    public String toString ()  {
-        String out = "Saldo: " + saldo
+    public String toString () {
+        String stringExample = "Saldo: " + attrNumericalToString(dominioSaldo, saldo)
                      + "\n Duração do empréstimo: " + duracao_emp
-                     + "\n Histórico de Crédito: " + hist_credito
+                     + "\n Histórico de Crédito: " + attrNumericalToString(dominioHistCredito, hist_credito)
                      + "\n Valor do empréstimo: " + valor
-                     + "\n Economias: " + economias
-                     + "\n Estado pessoal: " + est_pessoal
-                     + "\n Classe: " + classe;
-        return out;
+                     + "\n Economias: " + attrNumericalToString(dominioEconomias, economias)
+                     + "\n Estado pessoal: " + attrNumericalToString(dominioEstPessoal, est_pessoal)
+                     + "\n Classe: " + attrNumericalToString(dominioClasse, classe);
+        return stringExample;
     }
 }
--- Classificador/src/classificador/Utils.java
+++ Classificador/src/classificador/Utils.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package classificador;
 
 /**
@@ -10,20 +9,59 @@ package classificador;
  * @author igor
  */
 public class Utils {
-    
+
+    /**
+     * Retorna a representacao numerica de um valor de atributo nominal baseada
+     * no dominio desse atributo.
+     * 
+     * @param   domain o dominio do atributo
+     * @param   value  o valor do atributo
+     * @return         a representacao numerica de value.
+     */
+    public static int attrStringToNumerical(String[] domain, String value) throws Exception {
+        int intValue = 0;
+
+        //Testa se o valor do atributo e desconhecido
+        if (value.equals("?")) {
+            intValue = -1;
+        } else {
+            while (intValue < domain.length && !domain[intValue].equals(value))
+                intValue++;
+            if (intValue >= domain.length)
+                throw new Exception("Valor \"" + value + "\" nao pertence ao dominio." );
+        }
+
+        return intValue;
+    }
+
+    /**
+     * Retorna o valor do dominio a partir da representacao numerica de um
+     * atributo nominal.
+     * 
+     * @param   domain o dominio do atributo
+     * @param   value  a representacao numerica do valor do atributo
+     * @return         o valor mapeado no dominio por value.
+     */
+    public static String attrNumericalToString(String[] domain, int value) {
+        String strValue = "?";
+        if (value >= 0 && value < domain.length)
+            strValue = domain[value];
+        
+        return strValue;
+    }
+
     public static String getOption(String flag, String[] options) throws Exception {
         int i = 0;
         String readFlag = options[i];
-        
+
         while ((i < options.length) && !readFlag.equals(flag)) {
-             i = i + 2;
-             readFlag = options[i];
+            i = i + 2;
+            readFlag = options[i];
         }
-        
-        if (!readFlag.equals(flag))
+
+        if (!readFlag.equals(flag)) {
             throw new Exception("A opcao " + flag + " nao pode ser encontrada!");
-        
+        }
         return options[i + 1];
     }
-
 }
