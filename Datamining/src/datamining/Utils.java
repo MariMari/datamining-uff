ab9dc9d6348e472898133ad837550793e972066a
Igor Giusti da Silva <igor.giusti@corp.globo.com>
2009-04-24 18:25:43 -0300

    Limei os dois metodos estaticos de Utils que faziam conversao de valores de dominio. A classe Attribute ficou encarregada dessa conversao.

--- Datamining/src/datamining/Utils.java
+++ Datamining/src/datamining/Utils.java
@@ -10,61 +10,6 @@ package datamining;
  */
 public class Utils {
 
-    /**
-     * Retorna a representacao numerica de um valor de atributo nominal baseada
-     * no dominio desse atributo.
-     * 
-     * @param   domain o dominio do atributo
-     * @param   value  o valor do atributo
-     * @return         a representacao numerica de value.
-     */
-    public static int attrStringToNumerical(String[] domain, String value) throws Exception {
-        int intValue = 0;
-
-        //Testa se o valor do atributo e desconhecido
-        if (value.equals("?")) {
-            intValue = -1;
-        } else {
-            while (intValue < domain.length && !domain[intValue].equals(value))
-                intValue++;
-            if (intValue >= domain.length)
-                throw new Exception("Valor " + value + " nao pertence ao dominio." );
-        }
-
-        return intValue;
-    }
-
-    /**
-     * Retorna o valor do dominio a partir da representacao numerica de um
-     * atributo nominal.
-     * 
-     * @param   domain o dominio do atributo
-     * @param   value  a representacao numerica do valor do atributo
-     * @return         o valor mapeado no dominio por value.
-     */
-    public static String attrNumericalToString(String[] domain, int value) {
-        String strValue = "?";
-        if (value >= 0 && value < domain.length)
-            strValue = domain[value];
-        
-        return strValue;
-    }
-
-    public static String getOption(String flag, String[] options) throws Exception {
-        int i = 0;
-        String readFlag = options[i];
-
-        while ((i < options.length) && !readFlag.equals("-" + flag)) {
-            readFlag = options[i];
-            i = i + 2;
-        }
-
-        if (!readFlag.equals("-" + flag)) {
-            throw new Exception("A opcao " + flag + " nao pode ser encontrada!");
-        }
-        return options[i + 1];
-    }
-
     public static double entropy(Attribute attribute) throws Exception {
         int i,size;
         double probability,sum;
